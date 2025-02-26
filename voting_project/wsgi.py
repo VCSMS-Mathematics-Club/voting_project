332531c1afa56f206b81be02112842609c68e6b9
Engineed <teamengineed@gmail.com>
2025-02-26 21:51:06 +0800

    fix wsgi

--- voting_project/wsgi.py
+++ voting_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for voting_project project.
+WSGI config for backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'voting_project.settings')
+settings_module = 'voting_project.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'voting_project.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
