13a8dde173286fa9dd4367497799f4c6eeaba0c6
Engineed <teamengineed@gmail.com>
2025-02-26 21:50:16 +0800

    asgi fix

--- voting_project/asgi.py
+++ voting_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for voting_project project.
+ASGI config for backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'voting_project.settings')
+
+settings_module = 'voting_project.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'voting_project.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_asgi_application()
